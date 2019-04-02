608cd4005aa4ca1e0bcac4f7b71b9589b7bd21dc
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-04-02 19:19:03 -0400

    Added stars, years and directors

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -16,24 +16,30 @@ end
 def get_character_movies_from_api(character_name)
   counter = 1
   characters = get_characters("http://www.swapi.co/api/people/?page=#{counter}")
-  while characters["next"]
-   characters["results"].each do |character_hash|
-     if character_hash["name"].downcase == character_name
-       return list_of_films(character_hash)
-  else characters = nil
+  while characters
+    characters["results"].each do |character_hash|
+      if character_hash["name"].downcase == character_name
+        return list_of_films(character_hash)
+      end
+  end
+    if characters["next"]
+      characters = get_characters(characters["next"])
+    else characters = nil
   end
 end
-     counter += 1
+   counter += 1
 end
-end
+
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
   counter = 1
   films_hash.each do |film|
+    puts " "
     puts "#{counter}. #{film['title']}"
-    puts film['director']
-    puts "----"
+    puts "(dir. #{film['director']}, #{film['release_date'][0..3]})"
+    puts " "
+    puts "✨ ✨ ✨ ✨ ✨"
     counter += 1 # counter = counter + 1
   end
         puts "May the force be with you!"
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,10 @@
 def welcome
-  puts "Welcome!"
+  puts "Clearly, you're obsessed with Star Wars..."
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Who's your favorite character? 🤔"
+  puts " "
   gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
