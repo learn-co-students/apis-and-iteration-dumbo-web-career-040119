c7ff63db8d5d544b70e0ba0ccb701e00473ce03b
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-02 16:06:18 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,83 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+
 def get_character_movies_from_api(character_name)
+
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+
+  # response_string = RestClient.get("http://www.swapi.co/api/people/")
+  # response_hash = JSON.parse(response_string.body)
+  # puts JSON.pretty_generate(response_hash)
+
+  response_string = RestClient.get("https://www.swapi.co/api/people/")
+  response_hash = JSON.parse(response_string.body)
+
+  # while response_hash
+  #   find_character = response_hash["results"].find do |character_data|
+  #     # puts "#{character_data["name"]}     #{character_name}"
+  #     if character_name == character_data["name"].downcase
+  #     puts character_data["films"]
+  #       film = character_data["films"]
+  #       return film
+  #     end
+  #   end
+  #
+  #   if response_hash = response_hash["next"]
+  #     response_hash = JSON.parse(RestClient.get(response_hash).body)
+  #   else
+  #     nil
+  #   end
+
+
+while response_string
+  find_character = response_hash["results"].find do |character_data|
+    # puts "#{character_data["name"]}     #{character_name}"
+    if character_name == character_data["name"].downcase
+    puts character_data["films"]
+      film = character_data["films"]
+      return film
+    end
+  end
+
+  if response_string = RestClient.get(response_hash["next"])
+    response_hash = JSON.parse(response_string.body)
+  else
+    nil
+  end
+
+  # response_hash = response_hash["next"] ? JSON.parse(RestClient.get(response_hash["next"])) : nil
+end
+
+  # while character_hash
+  #     film_urls = character_hash["results"].find do |hash|
+  #       hash["name"].downcase == character
+  #     end
+  #     if film_urls
+  #       return film_urls["films"].map do |film|
+  #         JSON.parse(RestClient.get(film))
+  #       end
+  #     end
+  #     character_hash = character_hash["next"] ? JSON.parse(RestClient.get(character_hash["next"])) : nil
+  #   end
+  # end
+
+
+   # puts find_character
+
+#   find_film = find_character.each do |key, value|
+#      if key == "films"
+#        return value
+#      end
+# end
+# puts find_film
+  # response_hash.each do |key, value|
+  #
+  #   if key == response_hash["results"]['name'] && key == character_name
+  #     response_hash["results"]["films"]
+  #   end
+  # end
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -20,6 +93,23 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  # films = get_character_movies_from_api(character_name)
+  film_url = films.map do |film|
+    response_string = RestClient.get(film)
+    response_hash = JSON.parse(response_string.body)
+
+    title = response_hash["title"]
+    puts title
+    # return title
+  # title = response_hash["results"].find do |character_data|
+  #   # puts "#{character_data["name"]}     #{character_name}"
+  #   if character_name == character_data["name"].downcase
+  #   puts character_data["films"]
+  #     film = character_data["films"]
+  #     return film
+  #   end
+  end
+
 end
 
 def show_character_movies(character)
