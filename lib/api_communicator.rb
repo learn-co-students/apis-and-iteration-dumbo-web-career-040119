c3e40b6d6fddcd9b531a648e734e440dd1cb47a5
github username <github email address>
2019-04-02 16:26:26 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,8 +4,10 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  response_string = RestClient.get("http://www.swapi.co/api/people/")
+  response_hash = JSON.parse(response_string.body)
+  #puts JSON.pretty_generate(response_hash)
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -16,14 +18,44 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  find_character = response_hash["results"].find do |character_data|
+    character_data["name"] == character_name
+  end
+  i = 1
+  until find_character != nil
+    i += 1
+    response_string = RestClient.get("http://www.swapi.co/api/people/?page=#{i}")
+    response_hash = JSON.parse(response_string.body)
+
+    find_character = response_hash["results"].find do |character_data|
+      character_data["name"] == character_name
+    end
+  end
+  find_character["films"]
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  output = films.map do |link|
+    movie_string = RestClient.get(link)
+    movie_hash = JSON.parse(movie_string.body)
+    movie_hash["title"]
+  end
+  puts output
 end
+
+# def print_movies(films)
+#   # some iteration magic and puts out the movies in a nice list
+#   films.select do |link|
+#     movie_string = RestClient.get(link)
+#     movie_hash = JSON.parse(movie_string.body)
+#     puts movie_hash["title"]
+#   end
+# end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
+
   print_movies(films)
 end
 
