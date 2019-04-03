0f55932232185f49620e970fee2e7fbbc60517f7
Mallory Woods <malvwoods@gmail.com>
2019-04-03 09:07:21 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,8 +6,10 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   all_character_films = []
-  count = 1
-  response_string = RestClient.get("http://www.swapi.co/api/people/?page=1")
+  count = 0
+  while all_character_films.empty?
+    count += 1
+  response_string = RestClient.get("http://www.swapi.co/api/people/?page=#{count}")
   response_hash = JSON.parse(response_string)
   response_hash["results"].select do |info_hash|
      if info_hash["name"].downcase == character_name
@@ -17,12 +19,13 @@ def get_character_movies_from_api(character_name)
           film_response_hash = JSON.parse(film_string)
          all_character_films << film_response_hash
        end
+      end
     end
   end
   return all_character_films
 end
 
-###### with swapi search hack
+# <!---- with swapi search hack ---->
 
 # def get_character_movies_from_api(character_name)
 #   all_character_films = []
@@ -38,28 +41,17 @@ end
 #   end
 #   return all_character_films
 # end
-  #exitqbinding.pry
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 
+# <!---- END with swapi search hack ---->
 
 def print_movies(films)
   films.each do |movie_info|
     puts movie_info["title"]
   end
-  # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  #binding.pry
   print_movies(films)
 end
 
